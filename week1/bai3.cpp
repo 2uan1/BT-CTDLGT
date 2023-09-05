78ac5a4fb3aa36add8017501c206fcff847c5707
Phan Duc Quan <124783618+2uan1@users.noreply.github.com>
2023-09-05 10:43:11 +0700

    added bubble sort

--- week1/bai3.cpp
+++ week1/bai3.cpp
@@ -15,3 +15,19 @@ void selectionSort(int a[], int size)
         a[curMin] = tmp;
     }
 }
+
+void bubbleSort(int a[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size-i-1; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+            }
+        }
+    }
+}
